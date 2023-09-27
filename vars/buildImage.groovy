d5627b0b6cd364a6c9c603dcb9d7a1fc2bf872de
vascheffer <vascheffer@gmail.com>
2023-09-27 19:12:58 -0300

    Creating docker class

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,13 +1,6 @@
 #!/user/bin/env groovy
 
-// import com.example.Docker
+import com.example.Docker
 def call(String imageName) {
-    // return new Docker(this).buildDockerImage(imageName)
-        echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        // sh 'docker build -t vascheffer/demo-app:jma-2.0 .'
-         sh "docker build -t $imageName ."
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh "docker push $imageName"
-    }
+    return new Docker(this).buildDockerImage(imageName)
 }
--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,7 @@
 #!/user/bin/env groovy
 
 def call() {
-    echo "building the application for branch $BRANCH_NAME"
+    //echo "building the application for branch $BRANCH_NAME"
+    echo "building the application for branch $BRANCH_NAME" // branch_name is not working
     sh 'mvn package'
 }
