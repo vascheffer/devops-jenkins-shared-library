319a2bddda89a909e3ea3c8ddfc4178e2ef8b5e0
vascheffer <vascheffer@gmail.com>
2023-09-27 16:20:37 -0300

    Adjust buildImage function

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,13 @@
 #!/user/bin/env groovy
 
-import com.example.Docker
-def call(String imageName) {
-    return new Docker(this).buildDockerImage(imageName)
+// import com.example.Docker
+def call() {
+    // return new Docker(this).buildDockerImage(imageName)
+        echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t vascheffer/demo-app:jma-2.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push vascheffer/demo-app:jma-2.0'
+
+    }
 }
