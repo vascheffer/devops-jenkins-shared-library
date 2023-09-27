c234b18f595b79d34bf284e8bd0d5eccafead5d6
vascheffer <vascheffer@gmail.com>
2023-09-27 20:12:27 -0300

    hotfix

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -2,6 +2,6 @@
 
 def call() {
     //echo "building the application for branch $BRANCH_NAME"
-    echo "building the application for branch $BRANCH_NAME" // branch_name is not working
+    echo "building the application for branch ${env.BRANCH_NAME}" // branch_name is not working
     sh 'mvn package'
 }
