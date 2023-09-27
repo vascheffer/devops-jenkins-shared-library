90ca15e774a5ca39ce50fd824048dffd3f5f38f9
vascheffer <vascheffer@gmail.com>
2023-09-27 18:58:55 -0300

    Adding parameters for function

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,13 +1,13 @@
 #!/user/bin/env groovy
 
 // import com.example.Docker
-def call() {
+def call(String imageName) {
     // return new Docker(this).buildDockerImage(imageName)
         echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t vascheffer/demo-app:jma-2.0 .'
+        // sh 'docker build -t vascheffer/demo-app:jma-2.0 .'
+         sh "docker build -t $imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push vascheffer/demo-app:jma-2.0'
-
+        sh "docker push $imageName"
     }
 }
